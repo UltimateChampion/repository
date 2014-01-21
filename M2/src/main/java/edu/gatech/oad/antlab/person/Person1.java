1d34d45057982c8e6c25a1d7b4ac0fc346440d3b
harrisongalloway <harrisonbgalloway@gmail.com>
2014-01-21 13:55:43 -0500

    Completed Person1 sample method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,22 +3,22 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
+    /** Holds the persons real name */
+    private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
+    public Person1(String pname) {
+        name = pname;
+    }
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,8 +30,13 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+        //Person 1 put your implementation here
+		String first = input.substring(0,2);
+		String second = input.substring(2,input.length());
+		
+		String word = second + first;
+		
+        return word;
 	}
 	
 	/**
@@ -39,11 +44,12 @@ public class Person1 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+        return name + calc(input);
 	}
-
+	
 }
+
